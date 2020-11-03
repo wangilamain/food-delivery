4a7048ec158364c591e2d12acd42573cefe9caf6
Munge Ryan <mungeryan@gmail.com>
2020-11-03 17:43:53 +0300

    creating user tables

---
+++ app/models.py
@@ -0,0 +1,43 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from datetime import datetime
+from flask_login import UserMixin,current_user
+
+class User(db.Model,UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),unique = True,index = False,nullable = False)
+    email = db.Column(db.String(80),unique = True,index = False,nullable = False)
+    bio = db.Column(db.String(255),default = 'My default Bio')
+    hashed_password = db.Column(db.String(255),nullable = False)
+    order = db.relationship('Order',backref = 'user',lazy = 'dynamic')
+
+    @property 
+    def set_password(self):
+        raise AttributeError('You cant read the password attribute')
+
+    @set_password.setter
+    def password(self,password):
+        self.hashed_password = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.hashed_password,password)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer,primary_key = True)
+    
+
+
+    
