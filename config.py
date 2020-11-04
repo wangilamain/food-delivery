8a77a188c21f6602585a236b8b81af0dbc6b74d4
Munge Ryan <mungeryan@gmail.com>
2020-11-04 12:17:04 +0300

    configuring email

--- config.py
+++ config.py
@@ -3,8 +3,16 @@ from os import environ
 class Config:
     # General Config
     SECRET_KEY = environ.get('SECRET_KEY')
+    SECRET_KEY = 'Nonyadamnbusiness'
     # Database
     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:slowwhine@localhost/food'
+    # Mail
+    MAIL_PORT = 587
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = environ.get("MAIL_PASSWORD")
